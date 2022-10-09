00644272421c73ef3ffd2bc5b0e7fb0de47a42e5
cswccc <3344767250@qq.com>
2022-10-09 16:57:19 +0800

    master

---
+++ CMakeFiles/pro2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pro2.dir/src/pro2.cpp.o"
+  "CMakeFiles/pro2.dir/src/pro2.cpp.o.d"
+  "pro2"
+  "pro2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pro2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
